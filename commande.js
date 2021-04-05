ac49604d20a3a11f688fb6208327cd710ced815b
Tidushav3fun <69478381+Tidushav3fun@users.noreply.github.com>
2021-04-05 23:27:07 +0200

    Correction soucis importation ID

--- commande.js
+++ commande.js
@@ -1,12 +1,28 @@
+let panier = JSON.parse(localStorage.getItem("panier"))
+
+// Quantité de produits dans le panier //
+
+const positionElement3 = document.querySelector(".nombreproduits")
+
+if(panier === null){
+    QuantiteTotalHTML= '0';
+}else{
+    QuantiteTotalHTML = panier.length;
+}    
+
+positionElement3.innerHTML = `
+( ${QuantiteTotalHTML} )
+`;
+
 // Id pour le numéro de commande //
 let id = location.search.split('=')[1];
-let numeroCde = id.split('-')[0];
+let numeroCde = id;
 
 //  Total de la commande //
 function totalCommande(){
     sumTotal = 0; 
-    for(let i =0; i<products.length; i++){
-        sumTotal += products[i].price;
+    for(let i =0; i<panier.length; i++){
+        sumTotal += (panier[i].price/100);
     }
 
     return sumTotal
@@ -17,7 +33,7 @@ function showOrder (){
     let idOrder = document.getElementById('orderId');
     idOrder.innerHTML = numeroCde;
     let article = document.getElementById('nbrarticle');
-    article.innerHTML = "  (" + products.length + ") ";
+    article.innerHTML = "  (" + panier.length + ") ";
     let totalCde = document.getElementsByClassName('totalCde');
     totalCde[0].innerHTML = totalCommande() + " €";
     totalCde[1].innerHTML = totalCommande() + " €";
--- panier.js
+++ panier.js
@@ -91,91 +91,86 @@ positionElement4.addEventListener("click", function (event) {
 
 //                FORMULAIRE                    //
 
+// Objet contact à envoyer au serveur
+let contact = {
+    firstName:'',
+    lastName:'',
+    address:'',
+    city:'',
+    email:'',
+};
+
+
 // Création du formulaire  //
-let formValid = document.getElementById('ordre__validation');
-let nom = document.getElementById('nom');
-let prenom = document.getElementById('prenom');
-let email = document.getElementById('mail');
-let ville = document.getElementById('ville');
-let address = document.getElementById('address');
-let missNom = document.getElementById('missNom');
-let missPrenom = document.getElementById('missPrenom');
-let missEmail = document.getElementById('missEmail');
-let missVille = document.getElementById('missVille');
-let missAddress = document.getElementById('missAddress');
+let formValid = document.getElementById("validbutton");
+let nom = document.getElementById("nomform");
+let prenom = document.getElementById("prenom");
+let email = document.getElementById("Email");
+let ville = document.getElementById("Ville");
+let address = document.getElementById("adresse");
+let missNom = document.getElementById("missNom");
+let missPrenom = document.getElementById("missPrenom");
+let missEmail = document.getElementById("missEmail");
+let missVille = document.getElementById("missVille");
+let missAddress = document.getElementById("missAddress");
 let nomValid = /^[a-zA-ZéèîïÉÈÎÏ][A-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/; // Création de Regex // 
 let prenomValid = /^[a-zA-ZéèîïÉÈÎÏ][A-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 let emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 let villeValid = /^[a-zA-ZéèîïÉÈÎÏ][A-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 let addressValid = /^\d+\s[A-z]+\s[A-z]+/;
             
-formValid.addEventListener('click', validation); // Si les données du formulaires sont ok, Validation // 
+formValid.addEventListener("click", validation); // Si les données du formulaires sont ok, Validation // 
 
 
 function form(){
     //Si le champ nom est vide//
     if (nom.validity.valueMissing){
         missNom.textContent = 'Veuillez renseigner votre nom';
-        missNom.style.color = 'red'; 
     //Si le format de données est incorrect//
     }else if(nomValid.test(nom.value) == false){
         missNom.textContent = 'Format incorrect';
-        missNom.style.color = 'orange'; 
     }else{
         missNom.innerHTML = `<i class="fas fa-check"></i>`;
-        missNom.style.color = 'green';
     };
 
     //Si le champ prenom est vide//
     if (prenom.validity.valueMissing){
         missPrenom.textContent = 'Veuillez renseigner votre prénom';
-        missPrenom.style.color = 'red'; 
     //Si le format de données est incorrect//
     }else if (prenomValid.test(prenom.value) == false){
         missPrenom.textContent = 'Format incorrect';
-        missPrenom.style.color = 'orange';  
     }else{
         missPrenom.innerHTML = `<i class="fas fa-check"></i>`
-        missPrenom.style.color = 'green';
     };
     
     //Si le champ email est vide//
     if (email.validity.valueMissing){  
         missEmail.textContent = 'Veuillez renseigner votre e-mail';
-        missEmail.style.color = 'red';   
         //Si le format de données est incorrect//
     }else if (emailValid.test(email.value) == false){
         missEmail.textContent = 'Format incorrect';
-        missEmail.style.color = 'orange';  
     }else{
         missEmail.innerHTML = `<i class="fas fa-check"></i>`
-        missEmail.style.color = 'green';
     };
    
     //Si le champ ville est vide//
     if (ville.validity.valueMissing){
         missVille.textContent = 'Veuillez renseigner votre commune';
-        missVille.style.color = 'red';
         //Si le format de données est incorrect//
     }else if (villeValid.test(ville.value) == false){  
         missVille.textContent = 'Format incorrect';
-        missVille.style.color = 'orange';   
     }else{
         missVille.innerHTML = `<i class="fas fa-check"></i>`
-        missVille.style.color = 'green';
     };
    
     //Si le champ address est vide//
     if (address.validity.valueMissing){
         missAddress.textContent = 'Veuillez renseigner votre adresse';
-        missAddress.style.color = 'red';        
     //Si le format de données est incorrect//
     }else if (addressValid.test(address.value) == false){
         missAddress.textContent = 'Format incorrect';
-        missAddress.style.color = 'orange';  
     }else{
         missAddress.innerHTML = `<i class="fas fa-check"></i>`
-        missAddress.style.color = 'green';
     }; 
 };
     
@@ -196,20 +191,20 @@ function validation(){
         return false;
     }
 
-    // creation du tableau qui va contenir les id des peluches
+    // Creation du tableau qui va contenir les ID des peluches
     let productsId = [];
-    for(let i =0; i<products.length; i++){
-        productsId.push(products[i]._id)
+    for(let i =0; i<panier.length; i++){
+        productsId.push(panier[i].id)
     }
 
-    // creation de l objet a envoyer
+    // Creation de l objet a envoyer
 
     let objectToSend = {
         contact : contact,
         products : productsId,
     };
 
-    // envoi au serveur
+    // Envoi au serveur
 
     fetch("http://localhost:3000/api/teddies/order", {
         method: 'POST',            
@@ -221,7 +216,8 @@ function validation(){
     .then(response => { response.json()
         .then(function(json) {
             let orderId = json.orderId;
-            document.location.href = 'commande.html?id=' + orderId;  // envoi de l'ID de la commande dans l'url de la page de validation 
+            console.log(json)
+           document.location.href = 'commande.html?id=' + orderId;  // envoi de l'ID de la commande dans l'url de la page de validation 
         });
     })
     .catch(error => { // enregistrement si erreur lors de l'envoi de données 
